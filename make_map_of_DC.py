d000f7e734f2f2c81fba29bc3f2677525e5d2338
astronomerhunter <seanhunterbrooks@gmail.com>
2018-07-26 13:10:46 -0400

    .

--- make_map_of_DC.py
+++ make_map_of_DC.py
@@ -5,11 +5,7 @@ Usage:
 Options:
     --save
 """
-
-
 import time
-
-
 from docopt import docopt
 import requests
 import urllib.parse
@@ -31,8 +27,8 @@ def assemble_params(API_KEY):
     params_list = []
     CHANGE_IN_LAT = 0.001
     CHANGE_IN_LONG = 0.001
-    WIDTH = 1
-    HEIGHT = 1
+    WIDTH = 2
+    HEIGHT = 2
     for lat_index in range(0, WIDTH):
         lat = round(DC_LAT+lat_index*CHANGE_IN_LAT, 6)
         for long_index in range(0, HEIGHT):
@@ -56,31 +52,24 @@ def make_request(URL):
         else:
             time.sleep(current_delay)
             current_delay *= 2
-    raise Exception('Issue connecting to Google Maps API.')
+    raise Exception('Issue connecting to Google Maps API.') # TODO: Choose another exception class.
             
 
 def main(args):
-    API_KEY = args['API_KEY']
-    params_list = assemble_params(API_KEY)
+    params_list = assemble_params(args['API_KEY'])
     URL_list = params_to_URLs(params_list)
-    request_count = 0
     images = []
     for request_count in range(0, len(URL_list)):
         URL = URL_list[request_count]
         request = make_request(URL)
         image = Image.open(BytesIO(request.content))
         images.append(image)
-        image
-        
-        
-        
-    # if args['--save']:
-    #     OUTPUT_FILE_NAME = '{}.png'.format(request_count)
-    #     image = Image.open(BytesIO(request.content))
-    #     image.save(OUTPUT_FILE_NAME)
+        if args['--save']:
+            OUTPUT_FILE_NAME = '{}.png'.format(request_count)
+            print('saving {}...'.format(OUTPUT_FILE_NAME))
+            image.save(OUTPUT_FILE_NAME)
 
                         
-
 if __name__ == '__main__':
     arguments = docopt(__doc__, version='0.1')
     main(arguments)
